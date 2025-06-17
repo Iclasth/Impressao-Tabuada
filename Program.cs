3a8e5da77dcdea18618b7daf6bfd1ca7e748f8be
iclasTH <iclasinprogramacao@gmail.com>
2025-06-16 21:36:52 -0300

    Adcionanando os arquivos bases do projeto

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Tabuada
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Tab tabuada = new Tab();
+            Tab.ImprimirTabuadaDeDez();
+        }
+    }
+}
---
+++ Tab.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada
+{
+    public class Tab
+    {
+        public static void ImprimirTabuadaDeDez()
+        {
+            int x = 10;
+            Console.WriteLine("A seguir, a tabuada de 10:");
+           for (int cont = 0; cont < 11; cont++)
+            {
+                int result = x * cont;
+                Console.WriteLine($"{x} X {cont} = {result}");
+            }
+        }
+    }
+}
