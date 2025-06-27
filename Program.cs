a5388493e489d10795025b8e8daca862cd4cd422
iclasTH <iclasinprogramacao@gmail.com>
2025-06-27 14:24:17 -0300

    Adicionando um loop ao menu

--- Program.cs
+++ Program.cs
@@ -1,58 +1,64 @@
 ﻿using System;
+
 namespace Tabuada
 {
     public class Program
     {
         static void Main(string[] args)
         {
-           
-            
-            Console.WriteLine("Bem-vindo ao programa de tabuada!");
-            Console.WriteLine("Digite um numero de 1 a 10 no qual você deseja ver a tabuada: ");
-
-            switch (Console.ReadLine())
+            bool continuar = true;
+            while (continuar)
             {
-                case "1":
-                    Tab.ImprimirTabuadaDeUm();
-                    break;
-                case "2":
-                    Tab.ImprimirTabuadaDeDois();
-                    break;
-                case "3":
-                    Tab.ImprimirTabuadaDeTres();
-                    break;
-                case "4":
-                    Tab.ImprimirTabuadaDeQuatro();
-                    break;
-                case "5":
-                    Tab.ImprimirTabuadaDeCinco();
-                    break;
-                case "6":
-                    Tab.ImprimirTabuadaDeSeis();
-                    break;
-                case "7":
-                    Tab.ImprimirTabuadaDeSete();
-                    break;
-                case "8":
-                    Tab.ImprimirTabuadaDeOito();
-                    break;
-                case "9":
-                    Tab.ImprimirTabuadaDeNove();
-                    break;
-                case "10":
-                    Tab.ImprimirTabuadaDeDez();
-                    break;
-                default:
-                    Console.WriteLine("Número inválido. Por favor, digite um número de 1 a 10.");
-                    break;
+                Console.Clear();
+                Console.WriteLine("Bem-vindo ao programa de tabuada!");
+                Console.WriteLine("Digite um numero de 1 a 10 no qual você deseja ver a tabuada: ");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Tab.ImprimirTabuadaDeUm();
+                        break;
+                    case "2":
+                        Tab.ImprimirTabuadaDeDois();
+                        break;
+                    case "3":
+                        Tab.ImprimirTabuadaDeTres();
+                        break;
+                    case "4":
+                        Tab.ImprimirTabuadaDeQuatro();
+                        break;
+                    case "5":
+                        Tab.ImprimirTabuadaDeCinco();
+                        break;
+                    case "6":
+                        Tab.ImprimirTabuadaDeSeis();
+                        break;
+                    case "7":
+                        Tab.ImprimirTabuadaDeSete();
+                        break;
+                    case "8":
+                        Tab.ImprimirTabuadaDeOito();
+                        break;
+                    case "9":
+                        Tab.ImprimirTabuadaDeNove();
+                        break;
+                    case "10":
+                        Tab.ImprimirTabuadaDeDez();
+                        break;
+                    default:
+                        Console.WriteLine("Número inválido. Por favor, digite um número de 1 a 10.");
+                        break;
+                }
+
+                Console.WriteLine("\nDeseja ver a tabuada de outro número? (s/n)\n");
+                string resposta = Console.ReadLine().ToLower();
+                if (resposta != "s")
+                {
+                    continuar = false;
+                    Console.WriteLine("Obrigado por usar o programa de tabuada!");
+                }
+               
             }
-
-
-            Console.WriteLine("Pressione qualquer tecla para sair...");
-            Console.ReadKey();
-
-
         }
     }
-   
 }
